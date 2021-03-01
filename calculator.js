f3549c46d840e4f829f4250a487fb07cf4379c64
Alex Viktorov <viktorovaleksandr7@gmail.com>
2021-03-01 20:04:58 +0200

    add modules set

--- calculator.js
+++ calculator.js
@@ -2,13 +2,12 @@ const add = require('./core/add');
 const sub = require('./core/sub');
 const mult = require('./core/mult');
 const div = require('./core/div');
+const set = require('./core/set');
 
 module.exports = {
 	add,
 	sub,
 	mult,
 	div,
-	set:(number)=> {
-		return currentNumber = number;
-	}
+	set
 };
---
+++ core/set.js
@@ -0,0 +1,3 @@
+module.exports = (number) => {
+	return currentNumber = number;
+};
